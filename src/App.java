d38a8e86ee7ba43635152512c25f31beb42d3b75
Thyago Lucas <thyago.devin@gmail.com>
2023-01-10 11:15:18 -0400

    feat: multiplicaAi delivery

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Digite um numero a ser multiplicado");
+        
+        int numero = reader.nextInt();
+
+        System.out.println("Tabela de multiplicação de " + numero);
+
+        for(int i = 0; i <= 10; i++){
+            int resultado = numero * i;
+
+            System.out.println( "|" + numero + " x " + i + " = " + resultado + "|");
+        }
     }
 }
